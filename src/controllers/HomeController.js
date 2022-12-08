e3e02c02cdfd7459290afbffb12bd258691cb82c
Filippo Marinelli <lipo-x123@outlook.com>
2022-12-08 12:30:19 -0300

    creating alunos CRUD

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoaluno = await Aluno.create({
-      nome: 'joao',
-      sobrenome: 'marcos',
-      email: 'joao@gmail.com',
-      idade: 25,
-      peso: 88,
-      altura: 1.80,
-    });
-    res.json(novoaluno);
+    res.json('index');
   }
 }
 
